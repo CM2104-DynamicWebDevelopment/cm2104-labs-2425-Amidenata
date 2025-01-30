32b7347ebc7b4994d0af4fe2e49bd3b3d8652307
Amidenata <a.ndiaye-gaye@rgu.ac.uk>
2025-01-30 11:33:09 +0000

    Lab 11

--- lab_11/server.js
+++ lab_11/server.js
@@ -2,8 +2,8 @@ var express =  require('express');
 var app = express();
 
 app.get('/add', function(req, res){
-    var x = req.query.s;
-    var y = req.query.y;
+    var x = parseInt(req.query.s);
+    var y = parseInt(req.query.y);
     res.send("X + Y="+(x+y));
 });
 
