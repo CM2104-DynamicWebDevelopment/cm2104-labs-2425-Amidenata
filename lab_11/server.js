27c8085438cc811c999dca5e45e8e609960a20cb
Amidenata <a.ndiaye-gaye@rgu.ac.uk>
2025-01-30 12:15:00 +0000

    Lab 11 /getform

--- lab_11/server.js
+++ lab_11/server.js
@@ -51,4 +51,10 @@ app.get('/calc', function(req, res){
     res.send("Result:" + result);
 });
 
+app.get('/getform', function(req, res) {
+    var name = req.query.name;
+    var quest = req.query.quest;
+    res.send("Hi " + name + " I am sure you will " + quest);
+});
+
 app.listen(8080);
