5e8f8456411cebeb9c4c40aec8d04f00d7d8322e
Amidenata <a.ndiaye-gaye@rgu.ac.uk>
2025-01-30 11:41:18 +0000

    Lab 11

--- lab_11/server.js
+++ lab_11/server.js
@@ -1,10 +1,33 @@
 var express =  require('express');
 var app = express();
 
-app.get('/add', function(req, res){
-    var x = parseInt(req.query.x);
-    var y = parseInt(req.query.y);
-    res.send("X + Y="+(x+y));
+app.get('/calc', function(req, res){
+    var x = parseFloat(req.query.x);
+    var y = parseFloat(req.query.y);
+    var operator = req.query.operator;
+    var result;
+
+    switch(operator) {
+        case 'add':
+            result = x + y;
+            break;
+        case 'sub':
+            result = x - y;
+            break;
+        case 'mul':
+            result = x * y;
+            break;
+        case 'div':
+            if (y === 0) {
+                return res.send("Error: Division by zero is not allowed.");
+            }
+            result = x / y;
+            break;
+        default:
+            return res.send("Error: Invalid operator. Use add, sub, mul, or div.");
+    }
+
+    res.send("Result: ${result}");
 });
 
 app.listen(8080);
