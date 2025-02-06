b8b5e05424595b8246d177ae8a783e798d93e207
Amidenata <a.ndiaye-gaye@rgu.ac.uk>
2025-02-06 10:59:28 +0000

    Lab 12 access token

--- lab_12/server.js
+++ lab_12/server.js
@@ -12,6 +12,22 @@ var spotifyApi = new SpotifyWebApi({
     clientSecret: '6a03278a9df94716bce210b8446f8d15'
 });
 
+spotifyApi.clientCredentialsGrant().them(
+    function (data) {
+        console.log('The access token expires in ' + data.body['expires_in']);
+        console.log('The access token is ' + data.body['access_token']);
+
+        spotifyApi.setAccessToken(data.body['access_token']);
+
+    },
+    function (err) {
+        console.log(
+            'Something went wrong when retrieving an access token',
+            err.message
+        );
+    }
+);
+
 async function getTracks(searchterm, res) {
     spotifyApi.searchTracks(searchterm)
         .then(function (data) {
